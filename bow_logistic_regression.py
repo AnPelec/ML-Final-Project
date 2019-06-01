fd9b900361a3884c9a05a79b990da06ce7ec31f7
Angelos Pelecanos <AnPelec@users.noreply.github.com>
2019-06-01 04:33:45 -0400

    Add files via upload

---
+++ bow_logistic_regression.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
+
+df = pd.read_csv('Combined_News_DJIA.csv')
+
+n,d = df.shape #number of dates by number of columns
+
+corpus = [] #Corpus needs to be a list of strings for vectorizer
+
+#Remove punctuation, lowercase
+#Research other Headline/Stock SVM's to see what they did
+#Finding sentiment on headlines using pre-trained classifier
+#Measure correlation of sentiments with stock market changes
+#Shift the headlines versus the changes, see how long it takes market to respond
+
+y = np.zeros(n)
+for i in range(n):
+    heads = ""
+    y[i] = df.iloc[i,1]
+    for j in range(2,d): #Don't include labels/dates
+        if str(df.iloc[i,j]) != "nan":
+            heads += " "+df.iloc[i,j]
+        else:
+            heads += ""
+    corpus.append(heads)
+
+
+#vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=1)
+X = vectorizer.fit_transform(corpus)
+
+#clf = svm.SVC(kernel='linear', C=1)
+clf = LogisticRegression()
+print(sum(cross_val_score(clf, X, y, cv=5))/5)
---
+++ word_reader.py
@@ -0,0 +1,55 @@
+
+
+
+from string import punctuation, digits, printable
+import csv
+
+
+
+def extract_words(input_string):
+    """
+    Helper function for bag_of_words()
+    Inputs a text string
+    Returns a list of lowercase words in the string.
+    Punctuation and digits are separated out into their own words.
+    """
+    for c in punctuation + digits:
+        input_string = input_string.replace(c, ' ' + c + ' ')
+
+    # return [ps.stem(w) for w in input_string.lower().split()]
+    return input_string.lower().split()
+
+def bag_of_words(texts):
+    """
+    Inputs a list of string reviews
+    Returns a dictionary of unique unigrams occurring over the input
+
+    Feel free to change this code as guided by Section 3 (e.g. remove stopwords, add bigrams etc.)
+    """
+    dictionary = {} # maps word to unique index
+    for text in texts:
+        word_list = extract_words(text)
+        for word in word_list:
+            if word not in dictionary:
+                dictionary[word] = len(dictionary)
+    return dictionary
+
+def extract_bow_feature_vectors(reviews, dictionary):
+    """
+    Inputs a list of string reviews
+    Inputs the dictionary of words as given by bag_of_words
+    Returns the bag-of-words feature matrix representation of the data.
+    The returned matrix is of shape (n, m), where n is the number of reviews
+    and m the total number of entries in the dictionary.
+    """
+
+    num_reviews = len(reviews)
+    feature_matrix = np.zeros([num_reviews, len(dictionary)])
+
+    for i, text in enumerate(reviews):
+        word_list = extract_words(text)
+        for word in word_list:
+            if word in dictionary:
+                feature_matrix[i, dictionary[word]] = 1
+    # We want the feature vectors as columns
+    return feature_matrix.T
